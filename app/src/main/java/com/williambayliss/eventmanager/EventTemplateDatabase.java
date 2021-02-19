61caa86b604b799cf122bb846fa1057d91ec35bf
williambayliss <wmbayliss@gmail.com>
2021-02-18 20:33:17 -0500

    EventTemplateDatabase now extends RoomDatabase, finished layout in new_event_activity.xml

--- app/src/main/java/com/williambayliss/eventmanager/EventTemplateDatabase.java
+++ app/src/main/java/com/williambayliss/eventmanager/EventTemplateDatabase.java
@@ -1,8 +1,9 @@
 package com.williambayliss.eventmanager;
 
 import androidx.room.Database;
+import androidx.room.RoomDatabase;
 
 @Database(entities = {Event.class}, version = 1)
-public abstract class EventTemplateDatabase {
+public abstract class EventTemplateDatabase extends RoomDatabase {
     public abstract EventTemplateDAO eventTemplateDAO();
 }
--- app/src/main/java/com/williambayliss/eventmanager/NewEventActivity.java
+++ app/src/main/java/com/williambayliss/eventmanager/NewEventActivity.java
@@ -2,14 +2,23 @@ package com.williambayliss.eventmanager;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NewEventActivity extends AppCompatActivity {
-
+    Button setDateButton;
+    Button startTimeButton;
+    Button endTimeButton;
+    Button alertTypeButton;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
+
+        setDateButton = findViewById(R.id.date_button);
+        startTimeButton = findViewById(R.id.start_time_button);
+        endTimeButton = findViewById(R.id.end_time_button);
+        alertTypeButton = findViewById(R.id.alert_type_button);
     }
 }
