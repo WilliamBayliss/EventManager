74f22a7919ec4281589f9562679742a5f8b1c650
williambayliss <wmbayliss@gmail.com>
2021-02-23 17:45:03 -0500

    attached RecyclerItemClickListener to EventListActivity

--- app/src/main/java/com/williambayliss/eventmanager/EventListActivity.java
+++ app/src/main/java/com/williambayliss/eventmanager/EventListActivity.java
@@ -35,6 +35,19 @@ public class EventListActivity extends AppCompatActivity {
         recyclerView.setAdapter(eventListAdapter);
         recyclerView.setLayoutManager(layoutManager);
 
+        recyclerView.addOnItemTouchListener(
+                new RecyclerItemClickListener(getApplicationContext(), recyclerView, new RecyclerItemClickListener.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(View view, int position) {
+//                        TODO
+                    }
+
+                    @Override
+                    public void onLongItemClick(View view, int position) {
+//                        TODO
+                    }
+                }));
+
         ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleItemTouchCallBack);
         itemTouchHelper.attachToRecyclerView(recyclerView);
 
