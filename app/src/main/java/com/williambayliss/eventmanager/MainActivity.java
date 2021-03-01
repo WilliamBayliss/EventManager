455678a4dd72e9805af1b7afa3a890b33ea82762
williambayliss <wmbayliss@gmail.com>
2021-03-01 17:14:43 -0500

    replaced "new OnClickListener"s with lambdas

--- app/src/main/java/com/williambayliss/eventmanager/MainActivity.java
+++ app/src/main/java/com/williambayliss/eventmanager/MainActivity.java
@@ -1,15 +1,16 @@
 package com.williambayliss.eventmanager;
 
-import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.CalendarView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     FloatingActionButton newEventButton;
@@ -31,21 +32,13 @@ public class MainActivity extends AppCompatActivity {
         newEventButton = findViewById(R.id.new_event_button);
 
 //        Launches NewEventActivity
-        newEventButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                launchNewEventActivity();
-            }
-        });
+        newEventButton.setOnClickListener(v -> launchNewEventActivity());
 
 //        Will launch EventListActivity for selected Date on click
-        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            @Override
-            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                month = month + 1;
-                String selectedDate = String.format("%02d/%02d/%04d" , dayOfMonth, month, year);
-                launchEventListActivity(selectedDate);
-            }
+        calendarView.setOnDateChangeListener((view, year, month, dayOfMonth) -> {
+            month = month + 1;
+            String selectedDate = String.format(Locale.CANADA, "%02d/%02d/%04d" , dayOfMonth, month, year);
+            launchEventListActivity(selectedDate);
         });
     }
 
